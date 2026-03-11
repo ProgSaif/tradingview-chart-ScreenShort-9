585a7048e5e25462b01103352ca90299df012c3a
ProgSaif <isaifulone@gmail.com>
2026-03-11 13:19:31 +0600

    Update telegram_sender.py

--- telegram_sender.py
+++ telegram_sender.py
@@ -1,15 +1,8 @@
-import requests
+from telegram import Bot
 from config import BOT_TOKEN, CHANNEL_ID
 
+bot = Bot(token=BOT_TOKEN)
 
-def send_signal(message, image):
-
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto"
-
-    with open(image,"rb") as photo:
-
-        requests.post(
-            url,
-            data={"chat_id": CHANNEL_ID, "caption": message},
-            files={"photo": photo}
-        )
+def send_signal(message, chart_file):
+    with open(chart_file, 'rb') as f:
+        bot.send_photo(chat_id=CHANNEL_ID, photo=f, caption=message)
