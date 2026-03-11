b7c8a94bd91d3b1291544e89c395f67357d5c134
ProgSaif <isaifulone@gmail.com>
2026-03-11 12:42:07 +0600

    Update telegram_sender.py

--- telegram_sender.py
+++ telegram_sender.py
@@ -2,13 +2,14 @@ import requests
 from config import BOT_TOKEN, CHANNEL_ID
 
 
-def send_photo(photo_path):
+def send_signal(message, image):
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto"
 
-    with open(photo_path, "rb") as photo:
+    with open(image,"rb") as photo:
+
         requests.post(
             url,
-            data={"chat_id": CHANNEL_ID},
+            data={"chat_id": CHANNEL_ID, "caption": message},
             files={"photo": photo}
         )
